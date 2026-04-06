e02b0f60d6be1cbd0f9173a00c2de7e002edaa1d
blackword_2001 <blackword_2001@outlook.com>
2026-04-06 10:32:01 +0800

    2026/4/6-10:31

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,67 @@
+use std::fmt::format;
+
 use clap::Parser;
 use image::ImageFormat;
+
+#[derive(Debug, Clone)]
+enum ChannelInput {
+    Image(String),
+    Color(u8),
+}
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum PackMode {
+    RGB,
+    RGBA,
+}
 
 #[derive(Parser, Debug)]
 #[command(name = "texpack")]
 #[command(about = "轻量级 CLI 贴图打包工具")]
 struct Args {
-    input: String,
-    output: String,
+    #[arg(short = 'R')]
+    mode: PackMode,
+    args: Vec<String>,
+}
+
+fn parse_input(s:&str) -> Result<ChannelInput, String> {
+    match s.to_lowercase().as_str() {
+        "black" => return Ok(ChannelInput::Color(0)),
+        "white" => return Ok(ChannelInput::Color(255)),
+        _ => {}
+    }
+
+    if let Ok(v) = s.parse::<f64>() {
+        if !(0.0..=1.0).contains(&v) {
+            return Err(format!("数值{} 超出范围，必须在0-1之间", v));
+        }
+        return Ok(ChannelInput::Color((v * 255.0).round() as u8));
+    }
+
+    Ok(ChannelInput::Image(s.to_string()))
 }
 
 fn main() {
     let args = Args::parse();
 
-    println!("输入文件：{}", args.input);
-    println!("输出文件：{}", args.output);
+    let expected_count = match args.mode {
+        PackMode:: RGB => 4,
+        PackMode:: RGBA => 5,
+    };
 
-    let img = image::open(&args.input).expect("无法读取输入图片");
+    if args.args.len() != expected_count {
+        eprintln!("错误: {} 模式需要 {} 个输入参数 + 1 个输出路径",
+        format!("{:?}", args.mode), expected_count - 1);
 
-    println!(
-        "图片尺寸: {} x {}, 颜色类型:{:?}",
-        img.width(),
-        img.height(),
-        img.color()
-    );
+        std::process::exit(1);
+    }
 
-    img.save_with_format(&args.output, ImageFormat::Png)
-        .expect("无法保存输出图片");
+    let input_strs: Vec<&String> = args.args.iter().take(expected_count - 1).collect();
+    let output_path = args.args.last().unwrap();
 
-    println!("✅ 已保存到：{}",args.output);
+    println!("模式：{:?}", args.mode);
+    println!("输入：{:?}", input_strs);
+    println!("输出：{}", output_path);
+
+    
 }
