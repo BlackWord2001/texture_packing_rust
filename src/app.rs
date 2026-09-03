b468d92994e93dd60e6cc886e95b9d7e67e8f697
blackword <blackword_2001@outlook.com>
2026-09-04 02:36:28 +0800

    MenuBar!!!

--- src/app.rs
+++ src/app.rs
@@ -14,7 +14,7 @@ impl TexpackApp {
 impl eframe::App for TexpackApp {
     fn ui(&mut self, ctx: &mut egui::Ui, frame: &mut eframe::Frame) {
         ui::render_top_panel(ctx, self);
-        ui::render_left_panel(ctx, self);
         ui::render_central_panel(ctx, self);
+        // ui::render_left_panel(ctx, self);
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -5,17 +5,41 @@ use crate::app::TexpackApp;
 pub fn render_central_panel(ui: &mut egui::Ui, app: &mut TexpackApp) {
     egui::CentralPanel::default().show(ui, |ui| {
         ui.heading("central panel");
+
+        
     });
 }
 
 pub fn render_top_panel(ui: &mut egui::Ui, app: &mut TexpackApp) {
     egui::Panel::top("my top panel").show(ui, |ui| {
-        ui.label("top panel");
+        
+        egui::MenuBar::new().ui(ui, |ui| {
+            ui.menu_button("File", |ui| {
+                if ui.button("Open").clicked() {
+                    println!("Open clicked");
+                }
+    
+                if ui.button("Save").clicked() {
+                    println!("Save clicked");
+                }
+    
+                if ui.button("Exit").clicked() {
+                    println!("Exit clicked");
+                }
+            });
+
+            ui.menu_button("Preferences", |ui| {
+                if ui.button("Theme").clicked() {
+                    println!("select theme");
+                }
+            });
+        });
+
     });
 }
 
-pub fn render_left_panel(ui: &mut egui::Ui, app: & mut TexpackApp) {
-    egui::Panel::left("my left panel").show(ui, |ui| {
-        ui.label("left panel");
-    });
-}
+// pub fn render_left_panel(ui: &mut egui::Ui, app: &mut TexpackApp) {
+//     egui::Panel::left("my left panel").show(ui, |ui| {
+//         ui.label("left panel");
+//     });
+// }
